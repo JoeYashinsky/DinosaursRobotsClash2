61415a5e5c37890118aa5fa043ca8ce87306f8e2
JoeYashinsky <joey.yashinsky@gmail.com>
2020-11-21 14:53:26 -0500

    Added initial values for Weapon class in the constructor. Used Random class to generate a random number btw 10 and 20 that will serve as the robot weapon's attackPower.

--- Robos and Dinos Practice Proj/Weapon.cs
+++ Robos and Dinos Practice Proj/Weapon.cs
@@ -14,6 +14,12 @@ namespace Robos_and_Dinos_Practice_Proj
 
 
         // constructor (SPAWNER)
+        public Weapon(string type)                      //the type of weapon can be passed in, just like name for robot
+        {
+            this.type = type;
+            Random randomNumber = new Random();         //declared an instance of the Random class, and then below generating a rand number between 10 and 20. 
+            attackPower = randomNumber.Next(10, 21);    //// then storing that random # directly in the attackPower member variable.
+        }
 
 
 
