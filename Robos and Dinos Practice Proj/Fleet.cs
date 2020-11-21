1680b9d46d1700fe2772d934c4a9c8bad63fe8de
JoeYashinsky <joey.yashinsky@gmail.com>
2020-11-21 16:13:13 -0500

    created a List of robots in the Fleet class, then created three new robots to fill the Robot List, each with own name and weaponType.

--- Robos and Dinos Practice Proj/Fleet.cs
+++ Robos and Dinos Practice Proj/Fleet.cs
@@ -9,10 +9,19 @@ namespace Robos_and_Dinos_Practice_Proj
     class Fleet
     {
         //member variables (HAS A)
+        public List<Robot> robots;
 
 
 
         // constructor (SPAWNER)
+        public Fleet()
+        {
+            robots = new List<Robot>();       //make a new instance of the List. Otherwise, it would just be null to start. When Fleet is created, we want this List called 'robots' to exist.
+
+            Robot robotOne = new Robot("Tinker", "rocket");
+            Robot robotTwo = new Robot("Evers", "powerGlove");
+            Robot robotThree = new Robot("Chance", "bat");
+        }
 
 
 
--- Robos and Dinos Practice Proj/Weapon.cs
+++ Robos and Dinos Practice Proj/Weapon.cs
@@ -17,8 +17,8 @@ namespace Robos_and_Dinos_Practice_Proj
         public Weapon(string type)                      //the type of weapon can be passed in, just like name for robot
         {
             this.type = type;
-            Random randomNumber = new Random();         //declared an instance of the Random class, and then below generating a rand number between 10 and 20. 
-            attackPower = randomNumber.Next(10, 21);    //// then storing that random # directly in the attackPower member variable.
+            Random randomRoboNumber = new Random();         //declared an instance of the Random class, and then below generating a rand number between 10 and 20. 
+            attackPower = randomRoboNumber.Next(10, 21);    //// then storing that random # directly in the attackPower member variable.
         }
 
 
