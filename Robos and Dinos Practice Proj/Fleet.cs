8f1d2c0427bf17b3ed133efcb954c9e8f2fbae6c
JoeYashinsky <joey.yashinsky@gmail.com>
2020-11-21 16:48:37 -0500

    Finished building the Fleet with List of 3 dinos. Then built out the Herd class with a List of 3 different dinosaurs.

--- Robos and Dinos Practice Proj/Fleet.cs
+++ Robos and Dinos Practice Proj/Fleet.cs
@@ -21,6 +21,10 @@ namespace Robos_and_Dinos_Practice_Proj
             Robot robotOne = new Robot("Tinker", "rocket");
             Robot robotTwo = new Robot("Evers", "powerGlove");
             Robot robotThree = new Robot("Chance", "bat");
+            robots.Add(robotOne);
+            robots.Add(robotTwo);
+            robots.Add(robotThree);
+                                            //Whenever a Fleet gets constructed, want to instantiate its List inside the constructor, declare the Robot objects also in the constructor    
         }
 
 
--- Robos and Dinos Practice Proj/Herd.cs
+++ Robos and Dinos Practice Proj/Herd.cs
@@ -10,10 +10,24 @@ namespace Robos_and_Dinos_Practice_Proj
     {
 
         //member variables (HAS A)
+        public List<Dinosaur> dinosaurs;
 
 
 
         // constructor (SPAWNER)
+        public Herd()
+        {
+            dinosaurs = new List<Dinosaur>();
+
+            Dinosaur dinosaurOne = new Dinosaur("Jurassic Park Raptor in kitchen scene");
+            Dinosaur dinosaurTwo = new Dinosaur("Jurassic Park dino that killed Newman");
+            Dinosaur dinosaurThree = new Dinosaur("Jurassic World giant dino in water");
+            dinosaurs.Add(dinosaurOne);
+            dinosaurs.Add(dinosaurTwo);
+            dinosaurs.Add(dinosaurThree);
+        }
+        
+
 
 
 
