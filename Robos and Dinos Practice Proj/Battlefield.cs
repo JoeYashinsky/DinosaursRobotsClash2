b23d53f92ab7e1218ae5ca5da8498632ef2d1f44
JoeYashinsky <joey.yashinsky@gmail.com>
2020-11-22 00:47:49 -0500

    Wrote the logic for our FightToTheDeath method in the Battlefield class. Included an ability for dino to attack robo, vice-versa, and for these actions to continue looping unless one of the groups (Fleet or Herd) were without any more participants. At that point, the game would end.

--- Robos and Dinos Practice Proj/Battlefield.cs
+++ Robos and Dinos Practice Proj/Battlefield.cs
@@ -25,7 +25,39 @@ namespace Robos_and_Dinos_Practice_Proj
         // member methods (CAN DO)
         public void FightToTheDeath()
         {
-
+            while(herd.dinosaurs.Count > 0 && fleet.robots.Count > 0)
+            {
+                herd.dinosaurs[0].Attack(fleet.robots[0]);
+                Console.WriteLine("Rumble in the jungle! Dinosaurs attack!");
+            
+                if(fleet.robots[0].health <= 0)
+                {
+                    fleet.robots.RemoveAt(0);
+                    Console.WriteLine("Pour one out for that robot. RIP, robo-dude.");
+                    if(fleet.robots.Count == 0)
+                    {
+                        break;
+                    }    
+                }
+            
+                fleet.robots[0].Attack(herd.dinosaurs[0]);
+                Console.WriteLine("The robots have grabbed the momentum");
+            
+                if(herd.dinosaurs[0].health <= 0)
+                {
+                herd.dinosaurs.RemoveAt(0);
+                    Console.WriteLine("That dino just died a slow death...like The Lost World's performance at the box office.");
+                }
+            }
+            if(herd.dinosaurs.Count > 0)
+            {
+                Console.WriteLine("Life finds a way. Dinosaurs have won");
+            }
+            else
+            {
+                Console.WriteLine("Robots are victorioous. Celebratory screening of 'I, Robot' later tonight!");
+            }
+            
 
         }
 
